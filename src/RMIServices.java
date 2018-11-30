0f3f7660e32eb88cc04216232f33923bfe7a70d6
jmbds <jmbds@student.dei.uc.pt>
2018-11-30 22:19:21 +0000

    vamos rezar pode ser que funcione

--- src/RMIServices.java
+++ src/RMIServices.java
@@ -1,4 +1,5 @@
 import java.rmi.*;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public interface RMIServices extends Remote {
@@ -6,10 +7,19 @@ public interface RMIServices extends Remote {
     int login(String username, String password) throws RemoteException;
     int register(String username, String password) throws RemoteException;
     boolean insertMusic(String username, String password, String title, float duration, String lyrics, int interpreter_id, int composer_id, int album_id) throws RemoteException;
-    boolean insertAlbum(String title, int year, String description, String genre, int[] musicIDs, String publisher) throws RemoteException;
+    int insertAlbum(String title, int year, String description, String genre, int interpreter_id, String publisher) throws RemoteException;
     ArrayList<String[]> getPlaylists(String username) throws RemoteException;
     boolean newPlaylist(String username, String name, boolean priv, int [] musicIDs) throws RemoteException;
-    public ArrayList<String[]> getPlaylist(String name, String username) throws RemoteException;
+    ArrayList<String[]> getPlaylist(String name, String username) throws RemoteException;
     boolean writeReview(String username, int album, String review, int rate) throws RemoteException;
     boolean changePerks(String username) throws RemoteException;
+    int createComposer(String name, String description) throws RemoteException;
+    String[] searchComposer(String name) throws RemoteException;
+    String[] searchInterpreter(String name) throws RemoteException;
+    String[] searchAlbum(String name, int interpreter_id) throws RemoteException;
+    String[] searchAlbum(String name) throws RemoteException;
+    String[] getAlbumInfo(int id) throws RemoteException;
+    String[] searchMusic(String name) throws RemoteException;
+    int createBand(String name, String description, String date) throws RemoteException;
+    int createArtist(String name, String description, String date) throws RemoteException;
 }
--- src/Server.java
+++ src/Server.java
@@ -128,27 +128,244 @@ public class Server extends UnicastRemoteObject implements RMIServices {
         return privileges;
     }
 
-    public boolean insertAlbum(String title, int year, String description, String genre, int[] musicIDs, String publisher) {
-        String query = "INSERT INTO album (title, yearofpublication, description, genre) VALUES (\""+title+"\", "+year+", \""+description+"\", \""+genre+"\");";
+    public int insertAlbum(String title, int year, String description, String genre, int interpreter_id, String publisher) {
+        int album_id = -1;
 
-        if (!DBUpdate(query)) return false;
+        String query = "Select count(*)+1 from album";
+        ResultSet res = DBQuery(query);
+        try{
+            res.next();
+            album_id = res.getInt("count(*)+1");
+        }catch (SQLException e){}
 
-        //associar músicas ao álbum
-        for (int music : musicIDs) {
-            query = "INSERT INTO album_music (album_id, music_id) VALUES (SELECT ID FROM album WHERE title="+title+" AND description="+description+", "+ music +");";
-            if (!DBUpdate(query)) return false;
-        }
+        query = "INSERT INTO album (title, yearofpublication, description, genre, interpreter_id) VALUES (\""+title+"\", "+year+", \""+description+"\", \""+genre+"\", " + interpreter_id + ");";
+
+        if (!DBUpdate(query)) return -1;
 
         //associar publisher ao álbum
         query = "INSERT INTO album_publisher (album_id, publisher_name) VALUES ((SELECT ID FROM album WHERE title=\""+title+ "\" AND description=\"" + description+"\"), \"" + publisher+"\");";
-        if (!DBUpdate(query)) return false;
+        if (!DBUpdate(query)) return -1;
 
-        return true;
+        return album_id;
 
     }
 
+    public String[] searchMusic(String name){
+        String query = "Select count(*) FROM music, music_interpreter, interpreter WHERE interpreter.name LIKE \"%" + name + "%\" AND music_interpreter.interpreter_id = interpreter.id and music.id = music_interpreter.music_id";
+        ResultSet res = DBQuery(query);
+        int n = 0;
+        try{
+            res.next();
+            n = res.getInt("count(*)");
+        }catch(SQLException e){
+        }
+        if(n == 0)return null;
+        query = "Select music.id, music.title, interpreter.name FROM music, music_interpreter, interpreter WHERE interpreter.name LIKE \"%" + name + "%\" AND music_interpreter.interpreter_id = interpreter.id and music.id = music_interpreter.music_id";
+        res = DBQuery(query);
+
+        String reply[] = new String[n];
+        int i = 0;
+        try{
+            res.next();
+            while(!res.isAfterLast()){
+                reply[i] = "";
+                reply[i++] += res.getInt("music.id") + "/" + res.getString("music.title") + "/" + res.getString("interpreter.name");
+                res.next();
+            }
+        }catch (SQLException exc){}
+        return reply;
+    }
+
+    public String[] searchComposer(String name){
+        String query = "Select count(*) from composer where name like \"%" + name + "%\";";
+        ResultSet res = DBQuery(query);
+        int n = 0;
+        try{
+            res.next();
+            n = res.getInt("count(*)");
+        }catch(SQLException e){
+        }
+        if(n == 0)return null;
+        query = "Select id, name, description from composer where name like  \"%" + name + "%\";";
+        res = DBQuery(query);
+
+        String reply[] = new String[n];
+        int i = 0;
+        try{
+            res.next();
+            while(!res.isAfterLast()){
+                reply[i] = "";
+                reply[i++] += res.getInt("id") + "/" + res.getString("name") + "/" + res.getString("description");
+                res.next();
+            }
+        }catch (SQLException exc){}
+        return reply;
+    }
+
+
+
+     public String[] searchInterpreter(String name){
+         String query = "Select count(*) from interpreter where name like \"%" + name + "%\";";
+         ResultSet res = DBQuery(query);
+         int n = 0;
+         try{
+             res.next();
+             n = res.getInt("count(*)");
+         }catch(SQLException e){
+         }
+         if(n == 0)return null;
+         query = "Select id, name, description from interpreter where name like  \"%" + name + "%\";";
+         res = DBQuery(query);
+
+         String reply[] = new String[n];
+         int i = 0;
+         try{
+             res.next();
+             while(!res.isAfterLast()){
+                 reply[i] = "";
+                 reply[i++] += res.getInt("id") + "/" + res.getString("name") + "/" + res.getString("description");
+                 res.next();
+             }
+         }catch (SQLException exc){}
+         return reply;
+    }
+
+    public String[] searchAlbum(String name, int interpreter_id){
+        String query = "Select count(*) from album, interpreter where album.title like  \"%" + name + "%\"and interpreter.id = album.interpreter_id and interpreter.id = " + interpreter_id +";";
+        ResultSet res = DBQuery(query);
+        int n = 0;
+        try{
+            res.next();
+            n = res.getInt("count(*)");
+        }catch(SQLException e){
+        }
+        if(n == 0)return null;
+        query = "Select album.id, album.title, interpreter.name from album, interpreter where album.title like  \"%" + name + "%\" and interpreter.id = album.interpreter_id and interpreter.id = " + interpreter_id+ ";";
+        res = DBQuery(query);
+
+        String reply[] = new String[n];
+        int i = 0;
+        try{
+            res.next();
+            while(!res.isAfterLast()){
+                reply[i] = "";
+                reply[i++] += res.getInt("album.id") + "/" + res.getString("album.title") + "/" + res.getString("interpreter.name");
+                res.next();
+            }
+        }catch (SQLException exc){}
+        return reply;
+    }
+
+    public String[] searchAlbum(String name){
+
+        String query = "Select count(*) from album, interpreter where album.title like  \"%" + name + "%\"and interpreter.id = album.interpreter_id;";
+        ResultSet res = DBQuery(query);
+        int n = 0;
+        try{
+            res.next();
+            n = res.getInt("count(*)");
+        }catch(SQLException e){
+        }
+        query = "Select album.id, album.title, interpreter.name from album, interpreter where album.title like  \"%" + name + "%\" and interpreter.id = album.interpreter_id";
+        res = DBQuery(query);
+        if(n == 0)return null;
+        String reply[] = new String[n];
+        int i = 0;
+        try{
+            res.next();
+            while(!res.isAfterLast()){
+                reply[i] = "";
+                reply[i++] += res.getInt("album.id") + "/" + res.getString("album.title") + "/" + res.getString("interpreter.name");
+                res.next();
+            }
+        }catch (SQLException exc){return null;}
+        return reply;
+    }
+
+    public String[] getAlbumInfo(int id){
+            String[] reply = new String[2];
+            String query = "SELECT title from music, album_music WHERE album_id =" + id + " and music.id = album_music.music_id";
+            ResultSet res = DBQuery(query);
+            try{
+                res.next();
+                while(!res.isAfterLast()){
+                    reply[0] = "";
+                    reply[0] += res.getString("title") + "\n";
+                }
+            }catch (SQLException exc){
+            }
+            query = "SELECT genre, yearofpublication from album where album.id = " + id + ";";
+            res = DBQuery(query);
+            try{
+                res.next();
+                reply[1] = "";
+                reply[1] += res.getString("genre") + "/" + res.getInt("yearofpublication") + "/";
+            }catch (SQLException exc){
+
+            }
+            query = "SELECT round(AVG(rate),2) from review WHERE album_id = " + id + ";";
+            res =DBQuery(query);
+            try{
+                res.next();
+                reply[1] += res.getString("round(AVG(rate),2)");
+            }catch (SQLException e){
+                reply[1] += "NaN";
+            }
+            return reply;
+    }
+
+
+    public int createBand(String name, String description, String date){
+        int id = 1;
+        String query = "Select MAX(id)+1 from interpreter;";
+        ResultSet res = DBQuery(query);
+        try{
+            res.next();
+            id = res.getInt("MAX(id)+1");
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        query = "Insert into interpreter (name, description) values (" + name + " , " + description + ");";
+        if(!DBUpdate(query)) return -1;
+        query = "Insert into band (interpreter_id, dateofcreation) values (" + id + " , " + date + ");";
+        if(!DBUpdate(query)) return -1;
+        return id;
+    }
+
+    public int createArtist(String name, String description, String date){
+        int id = 1;
+        String query = "Select MAX(id)+1 from interpreter;";
+        ResultSet res = DBQuery(query);
+        try{
+            res.next();
+            id = res.getInt("MAX(id)+1");
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        query = "Insert into interpreter (name, description) values (" + name + " , " + description + ");";
+        if(!DBUpdate(query)) return -1;
+        query = "Insert into artist (interpreter_id, birthday) values (" + id + " , " + date + ");";
+        if(!DBUpdate(query)) return -1;
+        return id;
+    }
+
+    public int createComposer(String name, String description){
+        int id = 1;
+        String query = "Select MAX(id)+1 from composer;";
+        ResultSet res = DBQuery(query);
+        try{
+            res.next();
+            id = res.getInt("MAX(id)+1");
+        }catch(SQLException ex){
+            ex.printStackTrace();
+        }
+        query = "INSERT INTO composer (name, description) VALUES ('" + name + "' , '" + description + "');";
+        if(!DBUpdate(query)) return -1;
+        return id;
+    }
+
     public boolean insertMusic(String username, String password, String title, float duration, String lyrics, int interpreter_id, int composer_id, int album_id) throws RemoteException {
-        String query = "INSERT INTO music (title, duration, lyrics) VALUES (\""+title+"\", \""+duration+"\", \""+lyrics+"\");";
+        String query = "INSERT INTO music (title, duration, lyrics) VALUES ('"+title+"' , "+duration+", '"+lyrics + "');";
 
         if (!DBUpdate(query)) return false;
 
@@ -179,9 +396,10 @@ public class Server extends UnicastRemoteObject implements RMIServices {
     }
 
 
+
     public boolean connectDatabase() { //returns true if connection to DB is successful; false if else
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException exc) {
             System.out.println("JDBC Driver not found");
             return false;
